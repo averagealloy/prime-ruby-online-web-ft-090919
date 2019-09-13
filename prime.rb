b05fe2c757f9498b5692e897c1a619a833b5ad88
Mike McTighe <m.mctighe10@gmail.com>
2019-09-13 17:25:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,3 +5,17 @@
    end
   true
  end
+
+
+ def prime?(value)
+  if value <= 1
+    return false
+  elsif
+    (2..value - 1).each do |i|
+      if value % i == 0
+        return false
+      end
+    end
+  end
+  true
+ end
